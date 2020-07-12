46d919f443e696beaab0e252cdfeb02afcf664b6
AlveeM <alvee.akand@outlook.com>
2020-07-12 19:55:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    pr = params
+    @name = pr[:name]
+    @coach = pr[:coach]
+    @pg = pr[:pg]
+    @sg = pr[:sg]
+    @pf = pr[:pf]
+    @sf = pr[:sf]
+    @c = pr[:c]
+    erb :team
+  end
 
 end
